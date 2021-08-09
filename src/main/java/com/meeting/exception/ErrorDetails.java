75d9d4bdebab5bc4e6057353053983d38cdd96ad
Daniel <dancarsoa@gmail.com>
2021-08-09 19:16:15 -0300

    primeiro commit

---
+++ src/main/java/com/meeting/exception/ErrorDetails.java
@@ -0,0 +1,28 @@
+package com.meeting.exception;
+
+import java.util.Date;
+
+public class ErrorDetails {
+    final private Date timestamp;
+    final private String message;
+    final private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
---
+++ src/main/java/com/meeting/repository/RoomRepository.java
@@ -0,0 +1,10 @@
+package com.meeting.repository;
+
+import com.meeting.model.Room;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long> {
+
+}
